61a225a9476f03f7ec6b9378f753a59e2a520c75
EC2 Default User <ec2-user@ip-172-31-38-243.ec2.internal>
2019-07-28 10:57:54 +0000

    searchCity function changed to autocomplete input when typed in, calls citySelected function

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,12 +1,23 @@
+//Initialise map object for display, call function to search for city
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 48.857049, lng: 2.335665 },
         zoom: 8
     });
+    
+    searchCity();
 }
 
+//Search function for cities, using autocomplete from Google Places API
 function searchCity() {
-    var input = document.getElementById("city").value;
-    console.log(input);
+    var autocomplete = new google.maps.places.Autocomplete(document.getElementById("city"), {
+        types: ['(cities)']
+    });
+
+    autocomplete.addListener('place_changed', citySelected);
 }
 
+// Function to get city details and start search for details
+function citySelected() {
+    console.log(document.getElementById('city').value);
+}
