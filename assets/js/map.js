144143030994a44e7ad0d1c2ea1fd7b80e4ef80d
EC2 Default User <ec2-user@ip-172-31-38-243.ec2.internal>
2019-07-28 12:29:53 +0000

    city selected function with zoom to city added

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,23 +1,34 @@
+var autocomplete;
+var map;
+
 //Initialise map object for display, call function to search for city
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 48.857049, lng: 2.335665 },
         zoom: 8
     });
-    
+
     searchCity();
 }
 
-//Search function for cities, using autocomplete from Google Places API
+//Search function for cities, using autocomplete from Google Places API -- adapted from https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
 function searchCity() {
-    var autocomplete = new google.maps.places.Autocomplete(document.getElementById("city"), {
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById("city"), {
         types: ['(cities)']
     });
 
     autocomplete.addListener('place_changed', citySelected);
 }
 
-// Function to get city details and start search for details
+// Function to get city details and zoom to city  -- adapted from https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
 function citySelected() {
-    console.log(document.getElementById('city').value);
+    var city = autocomplete.getPlace();
+    console.log(`the city is ${city.geometry.location}`);
+
+    if (city.geometry) {
+        map.panTo(city.geometry.location);
+        map.setZoom(10);
+    } else {
+        document.getElementById("city").placeholder = 'Enter a city';
+    }
 }
