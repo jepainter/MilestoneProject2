78f6430bc7c2caf1865b87e0c4727d7347215112
EC2 Default User <ec2-user@ip-172-31-38-243.ec2.internal>
2019-07-28 09:03:14 +0000

    added basic searchCity function to retrieve and log city search

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,12 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 48.857049, lng: 2.335665 },
         zoom: 8
     });
 }
+
+function searchCity() {
+    var input = document.getElementById("city").value;
+    console.log(input);
+}
+
