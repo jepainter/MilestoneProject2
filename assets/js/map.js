7647c7a373a41e782f32a5d57a6d3923ab00754e
EC2 Default User <ec2-user@ip-172-31-38-243.ec2.internal>
2019-07-30 08:51:30 +0000

    changed getAttraction function to receive input type for searches

--- assets/js/map.js
+++ assets/js/map.js
@@ -28,7 +28,7 @@ function citySelected() {
     if (city.geometry) {
         map.panTo(city.geometry.location);
         map.setZoom(12);
-        getAttractions(city);
+        getAttractions("attractions");
     }
     else {
         document.getElementById("city").placeholder = 'Enter a city';
@@ -36,15 +36,15 @@ function citySelected() {
 }
 
 // Function to get attractions within a specified city -- adapted from https://developers.google.com/maps/documentation/javascript/examples/place-search
-function getAttractions(detail) {
-//    console.log(`City: ${detail.geometry.location}`);
+function getAttractions(type) {
+    //    console.log(`City: ${detail.geometry.location}`);
 
     var attractionAreaType = {
         bounds: map.getBounds(),
-        types: ['casino']
+        types: [type]
     };
 
-    console.log(`Bounds: ${attractionAreaType.bounds}`);
+    console.log(`Bounds: ${attractionAreaType.types}`);
 
     var attractions = new google.maps.places.PlacesService(map);
 
@@ -55,13 +55,13 @@ function getAttractions(detail) {
             }
 
             map.setCenter(results[0].geometry.location);
-//            console.log(results.length);
-//            console.log(results[1]);
-//Call populate table function
+            //            console.log(results.length);
+            //            console.log(results[1]);
+            //Call populate table function
             populateTable(results);
         }
     });
-    
+
 }
 
 // Marker creation function -- adapted from https://developers.google.com/maps/documentation/javascript/examples/place-search
@@ -73,139 +73,33 @@ function createMapMarker(attraction) {
 
     google.maps.event.addListener(marker, 'click', function() {
         var info = new google.maps.InfoWindow();
-        
-//        console.log(`Place: ${attraction.name}`);
-        
+
+        //        console.log(`Place: ${attraction.name}`);
+
         info.setContent(attraction.name);
         info.open(map, this);
     });
 }
 
 // Populate table function to display search results
-function populateTable(data){
+function populateTable(data) {
     var category = document.getElementById("attractions");
     category.innerHTML = ``;
-    
+
     // Table headers
-    var attractionHeaders = [`<td>Name</td>`,`<td>Type</td>`,`<td>Address</td>`,`<td>Rating</td>`,`<td>Website</td>`];
-    var barsRestaurantHeaders = [`<td>Name</td>`,`<td>Type</td>`,`<td>Address</td>`,`<td>Rating</td>`,`<td>Phone</td>`];
-    var accommodationHeaders = [`<td>Name</td>`,`<td>Rating</td>`,`<td>Phone</td>`,`<td>Website</td>`];
-    
-    var dataRow = [];
-    var tableRow = [];
-        for (var i=0;i<data.length;i++){
-            dataRow.push(`<td>${data[i].name}</td>`);
-            dataRow.push(`<td>${data[i].types[0]}</td>`);
-            dataRow.push(`<td>${data[i].vicinity}</td>`);
-            dataRow.push(`<td>${data[i].rating}</td>`);
-            dataRow.push(`<td>url</td>`);
-            tableRow.push(`<tr>${dataRow}</tr>`);
-            dataRow = [];
-        }
-       console.dir(data);
-        
-    category.innerHTML += `<table>${attractionHeaders}${tableRow}</table>`;
-}var autocomplete;
-var map;
+    var attractionHeaders = `<th>Name</th><th>Type</th><th>Address</th><th>Rating</th><th>Website</th>`;
+    var restaurantHeaders = `<th>Name</th><th>Type</th><th>Address</th><th>Rating</th><th>Phone</th>`;
+    var barHeaders = `<th>Name</th><th>Type</th><th>Address</th><th>Rating</th><th>Phone</th>`;
+    var accommodationHeaders = `<th>Name</th><th>Rating</th><th>Phone</th><th>Website</th>`;
 
-//Initialise map object for display, call function to search for city
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 48.857049, lng: 2.335665 },
-        zoom: 8
-    });
-
-    searchCity();
-}
-
-//Search function for cities, using autocomplete from Google Places API -- adapted from https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
-function searchCity() {
-    autocomplete = new google.maps.places.Autocomplete(document.getElementById("city"), {
-        types: ['(cities)']
-    });
-
-    autocomplete.addListener('place_changed', citySelected);
-}
-
-// Function to get city details and zoom to city  -- adapted from https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
-function citySelected() {
-    var city = autocomplete.getPlace();
-    console.log(`the city is ${city.geometry.location}`);
-    //Try catch to be inserted for error handling
-    if (city.geometry) {
-        map.panTo(city.geometry.location);
-        map.setZoom(12);
-        getAttractions(city);
-    }
-    else {
-        document.getElementById("city").placeholder = 'Enter a city';
-    }
-}
-
-// Function to get attractions within a specified city -- adapted from https://developers.google.com/maps/documentation/javascript/examples/place-search
-function getAttractions(detail) {
-//    console.log(`City: ${detail.geometry.location}`);
-
-    var attractionAreaType = {
-        bounds: map.getBounds(),
-        types: ['casino']
-    };
-
-    console.log(`Bounds: ${attractionAreaType.bounds}`);
-
-    var attractions = new google.maps.places.PlacesService(map);
-
-    attractions.nearbySearch(attractionAreaType, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMapMarker(results[i]);
-            }
-
-            map.setCenter(results[0].geometry.location);
-//            console.log(results.length);
-//            console.log(results[1]);
-//Call populate table function
-            populateTable(results);
-        }
-    });
-    
-}
-
-// Marker creation function -- adapted from https://developers.google.com/maps/documentation/javascript/examples/place-search
-function createMapMarker(attraction) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: attraction.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        var info = new google.maps.InfoWindow();
-        
-//        console.log(`Place: ${attraction.name}`);
-        
-        info.setContent(attraction.name);
-        info.open(map, this);
-    });
-}
-
-// Populate table function to display search results
-function populateTable(data){
-    var category = document.getElementById("attractions");
-    category.innerHTML = ``;
-    
-    // Table headers
-    var attractionHeaders = [`<th>Name</th><th>Type</th><th>Address</th><th>Rating</th><th>Website</th>`];
-    var barsRestaurantHeaders = [`<td>Name</td>`,`<td>Type</td>`,`<td>Address</td>`,`<td>Rating</td>`,`<td>Phone</td>`];
-    var accommodationHeaders = [`<td>Name</td>`,`<td>Rating</td>`,`<td>Phone</td>`,`<td>Website</td>`];
-    
     var dataRow;
     var tableRow = ``;
-        for (var i=0;i<data.length;i++){
-            dataRow = `<td>${data[i].name}</td><td>${data[i].types[0]}</td><td>${data[i].vicnity}</td><td>${data[i].rating}</td><td>url</td>`;
-            tableRow += `<tr>${dataRow}</tr>`;
-            dataRow = ``;
-        }
- // console.dir(data);
-       
+    for (var i = 0; i < data.length; i++) {
+        dataRow = `<td>${data[i].name}</td><td>${data[i].types[0]}</td><td>${data[i].vicinity}</td><td>${data[i].rating}</td><td>url</td>`;
+        tableRow += `<tr>${dataRow}</tr>`;
+        dataRow = ``;
+    }
+    console.dir(data);
+
     category.innerHTML = `<table>${attractionHeaders}${tableRow}</table>`;
 }
