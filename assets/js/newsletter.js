2385396b8bda7e58a2a4f1bb1d9e27b3b4b669c7
EC2 Default User <ec2-user@ip-172-31-38-243.ec2.internal>
2019-08-08 06:36:47 +0000

    fixed auto close of modal after successfull submit, clear form after submission

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -6,10 +6,17 @@ function sendEmail(contactInfo){
     })
     .then (
         function(response){
-            alert("Email was successfully sent, you can close the sign-up form.", response);
+            alert("Newsletter sign up was successfully sent.", response);
+            $('#newsletterModal').modal('hide');
+           
         },
         function(error){
-           alert("Error: Email not sent, please try again later.", error); 
+           alert("Error: Email not sent, please try again.", error); 
         });
         return false;
 }
+
+//JQuery to clear modal content after submission -- adapted from https://stackoverflow.com/questions/21151044/how-to-clear-all-input-fields-in-bootstrap-modal-when-clicking-data-dismiss-butt/35079811
+$('#newsletterModal').on('hidden.bs.modal', function () {
+    $(this).find('form').trigger('reset');
+})
